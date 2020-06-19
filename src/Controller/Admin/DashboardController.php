b43c8c679133173f9daabe7354aec9813e3ba97c
Jan Böhmer <jan.h.boehmer@gmx.de>
2020-06-19 22:58:32 +0200

    Added an dashboard page.

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -11,6 +11,8 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Config\UserMenu;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use EasyCorp\Bundle\EasyAdminBundle\Factory\MenuFactory;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 class DashboardController extends AbstractDashboardController
@@ -26,6 +28,15 @@ class DashboardController extends AbstractDashboardController
     {
         return Dashboard::new()
             ->setTitle('StuRa Finanzen');
+    }
+
+    /**
+     * @Route("/admin")
+     * @return Response
+     */
+    public function index(): Response
+    {
+        return $this->render('admin/dashboard.html.twig');
     }
 
     public function configureCrud(): Crud
