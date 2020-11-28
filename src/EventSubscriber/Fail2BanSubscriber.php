30318aa3a7639cdd9cf6ebae70a2d54a95ecb746
Jan Böhmer <jan.h.boehmer@gmx.de>
2020-11-28 23:52:05 +0100

    Added fail2ban logging.

---
+++ src/EventSubscriber/Fail2BanSubscriber.php
@@ -0,0 +1,68 @@
+<?php
+/*
+ * Copyright (C) 2020  Jan Böhmer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace App\EventSubscriber;
+
+
+use Psr\Log\LoggerInterface;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Security\Core\AuthenticationEvents;
+use Symfony\Component\Security\Core\Event\AuthenticationFailureEvent;
+
+/**
+ * This event subscriber
+ */
+class Fail2BanSubscriber implements EventSubscriberInterface
+{
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @var RequestStack
+     */
+    private $request;
+
+    /**
+     * @param LoggerInterface $logger
+     * @param RequestStack $request
+     */
+    public function __construct(LoggerInterface $fail2banLogger, RequestStack $request)
+    {
+        $this->logger = $fail2banLogger;
+        $this->request = $request;
+    }
+
+    public function logFail2Ban(AuthenticationFailureEvent $event): void
+    {
+        $ipAddress = $this->request->getCurrentRequest()->getClientIp();
+        $this->logger->error('Authentication failed for IP: %s' . $ipAddress);
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            AuthenticationEvents::AUTHENTICATION_FAILURE => [
+                'logFail2Ban'
+            ]
+        ];
+    }
+}
