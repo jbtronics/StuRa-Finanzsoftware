4545fa69e9a82c3aea8b98a5c478d609d732643c
Jan Böhmer <jan.h.boehmer@gmx.de>
2021-01-06 19:00:39 +0100

    Use correct timezone when showing datetimes.

---
+++ src/Helpers/UTCDateTimeType.php
@@ -0,0 +1,76 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * This file is part of Part-DB (https://github.com/Part-DB/Part-DB-symfony).
+ *
+ * Copyright (C) 2019 Jan Böhmer (https://github.com/jbtronics)
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ */
+
+namespace App\Helpers;
+
+use DateTime;
+use DateTimeZone;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\ConversionException;
+use Doctrine\DBAL\Types\DateTimeType;
+
+/**
+ * This DateTimeType all dates to UTC, so it can be later used with the timezones.
+ * Taken from here: https://www.doctrine-project.org/projects/doctrine-orm/en/2.6/cookbook/working-with-datetime.html.
+ */
+class UTCDateTimeType extends DateTimeType
+{
+    private static $utc_timezone;
+
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        if (!self::$utc_timezone) {
+            self::$utc_timezone = new DateTimeZone('UTC');
+        }
+
+        if ($value instanceof DateTime) {
+            $value->setTimezone(self::$utc_timezone);
+        }
+
+        return parent::convertToDatabaseValue($value, $platform);
+    }
+
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        if (!self::$utc_timezone) {
+            self::$utc_timezone = new DateTimeZone('UTC');
+        }
+
+        if (null === $value || $value instanceof DateTime) {
+            return $value;
+        }
+
+        $converted = DateTime::createFromFormat(
+            $platform->getDateTimeFormatString(),
+            $value,
+            self::$utc_timezone
+        );
+
+        if (!$converted) {
+            throw ConversionException::conversionFailedFormat($value, $this->getName(), $platform->getDateTimeFormatString());
+        }
+
+        return $converted;
+    }
+}
