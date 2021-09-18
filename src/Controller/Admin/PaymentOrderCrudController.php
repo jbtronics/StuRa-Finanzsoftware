d95e726a5566187b9c3eb1999be9eec52d873b6b
Jan Böhmer <jan.h.boehmer@gmx.de>
2021-09-18 10:35:34 +0200

    Made panels in paymentorder crud collapsibile.

--- src/Controller/Admin/PaymentOrderCrudController.php
+++ src/Controller/Admin/PaymentOrderCrudController.php
@@ -396,12 +396,12 @@ class PaymentOrderCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         //Documents
-        $documentsPanel = FormField::addPanel('payment_order.group.documents');
+        $documentsPanel = FormField::addPanel('payment_order.group.documents')->collapsible();
         $printed_form = VichyFileField::new('printed_form_file', 'payment_order.printed_form.label');
         $references = VichyFileField::new('references_file', 'payment_order.references.label');
 
         //Basic informations
-        $infoPanel = FormField::addPanel('payment_order.group.info');
+        $infoPanel = FormField::addPanel('payment_order.group.info')->collapsible();
         $id = IntegerField::new('id', 'payment_order.id.label');
         $firstName = TextField::new('first_name', 'payment_order.first_name.label');
         $lastName = TextField::new('last_name', 'payment_order.last_name.label');
@@ -447,7 +447,7 @@ class PaymentOrderCrudController extends AbstractCrudController
         //$creationDate = TextField::new('creation_date', 'creation_date');
 
         //Status informations
-        $statusPanel = FormField::addPanel('payment_order.group.status');
+        $statusPanel = FormField::addPanel('payment_order.group.status')->collapsible();
         $mathematicallyCorrect = BooleanField::new('mathematically_correct', 'payment_order.mathematically_correct.label')
             ->setHelp('payment_order.mathematically_correct.help')
             //Disable fields (and show coloumns as read only tags) if user does not have proper permissions to change
@@ -466,14 +466,14 @@ class PaymentOrderCrudController extends AbstractCrudController
         $references_exported = BooleanField::new('references_exported', 'payment_order.references_exported.label');
 
         //Payee informations
-        $payeePanel = FormField::addPanel('payment_order.group.receiver');
+        $payeePanel = FormField::addPanel('payment_order.group.receiver')->collapsible();
         $bankInfoAccountOwner = TextField::new('bank_info.account_owner', 'bank_info.account_owner.label');
         $bankInfoStreet = TextField::new('bank_info.street', 'bank_info.street.label');
         $bankInfoZipCode = TextField::new('bank_info.zip_code', 'bank_info.zip_code.label');
         $bankInfoCity = TextField::new('bank_info.city', 'bank_info.city.label');
 
         //Payee bank account infos
-        $bankInfoPanel = FormField::addPanel('payment_order.group.bank_info');
+        $bankInfoPanel = FormField::addPanel('payment_order.group.bank_info')->collapsible();
         $bankInfoIban = TextField::new('bank_info.iban', 'bank_info.iban.label');
         $bankInfoBic = TextField::new('bank_info.bic', 'bank_info.bic.label')
             ->setRequired(false)
