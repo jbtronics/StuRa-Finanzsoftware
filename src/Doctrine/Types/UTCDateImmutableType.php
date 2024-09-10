ef8d108c048186becc465f6468244811ee54eeb8
Jan Böhmer <mail@jan-boehmer.de>
2024-09-10 23:50:53 +0200

    Use their own classes for UTC correct date types

---
+++ src/Doctrine/Types/UTCDateImmutableType.php
@@ -0,0 +1,74 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace App\Doctrine\Types;
+
+use DateTime;
+use DateTimeZone;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\DateImmutableType;
+use Doctrine\DBAL\Types\DateType;
+use Doctrine\DBAL\Types\Exception\InvalidFormat;
+
+class UTCDateImmutableType extends DateImmutableType
+{
+    private static ?DateTimeZone $utc_timezone = null;
+
+    /**
+     * {@inheritdoc}
+     *
+     * @param T $value
+     *
+     * @return (T is null ? null : string)
+     *
+     * @template T
+     */
+    public function convertToDatabaseValue($value, AbstractPlatform $platform): ?string
+    {
+        if (!self::$utc_timezone instanceof \DateTimeZone) {
+            self::$utc_timezone = new DateTimeZone('UTC');
+        }
+
+        if ($value instanceof \DateTimeImmutable) {
+            $value->setTimezone(self::$utc_timezone);
+        }
+
+        return parent::convertToDatabaseValue($value, $platform);
+    }
+
+    /**
+     * {@inheritDoc}
+     *
+     * @param T $value
+     *
+     * @template T
+     */
+    public function convertToPHPValue($value, AbstractPlatform $platform): ?\DateTimeImmutable
+    {
+        if (!self::$utc_timezone instanceof \DateTimeZone) {
+            self::$utc_timezone = new DateTimeZone('UTC');
+        }
+
+        if (null === $value || $value instanceof \DateTimeImmutable) {
+            return $value;
+        }
+
+        $converted = \DateTimeImmutable::createFromFormat(
+            '!' . $platform->getDateFormatString(),
+            $value,
+            self::$utc_timezone
+        );
+
+        if (!$converted) {
+            throw InvalidFormat::new(
+                $value,
+                static::class,
+                $platform->getDateFormatString(),
+            );
+        }
+
+        return $converted;
+    }
+}
