0d9d841abfcd12656e199e95dc7bf2c6265bb251
Jan Böhmer <mail@jan-boehmer.de>
2024-10-04 23:57:16 +0200

    Moved templates logic to its own stimulus controller

--- assets/controllers/templates_controller.js
+++ assets/controllers/templates_controller.js
@@ -4,8 +4,8 @@ const STORAGE_KEY = "payment_order_templates";
 
 export default class extends Controller {
 
-    static targets = ['menu', 'delete_menu'];
-
+    static targets = ['menu', 'delete_menu', 'new_name'];
+    static values = {transNotEmpty: String, transDeleteConfirm: String}
 
 
     _templates = [];
@@ -28,14 +28,83 @@ export default class extends Controller {
         console.debug("Found templates: ", this._templates.length);
     }
 
-    deserializeForm(data)
-    {
-        alert(data);
-    }
 
     _saveTemplatesToStorage()
     {
         localStorage.setItem(STORAGE_KEY, JSON.stringify(this._templates));
+    }
+
+    /**
+     * Serialize the current form into a JSON object
+     * @return {{}}
+     * @private
+     */
+    _serializeForm()
+    {
+        let data = {};
+        let inputs = this.element.querySelectorAll('input[data-json]');
+        for (let input of inputs) {
+            //Only add the input to the data if it has a value
+            if (input.value) {
+                data[input.dataset.json] = input.value;
+            }
+        }
+
+        return data;
+    }
+
+    /**
+     * Deserialize the given object and fill the current form with the data
+     * @param data
+     */
+    deserializeForm(data)
+    {
+        for (const [key, value] of Object.entries(data)) {
+            let input = this.element.querySelector(`input[data-json="${key}"]`);
+            if (input) {
+                input.value = value;
+            }
+        }
+    }
+
+    /**
+     * Save the current form content as a template in the browser local storage
+     */
+    saveTemplate()
+    {
+        let name = this.new_nameTarget.value.trim();
+        if (!name) {
+            alert(this.transNotEmptyValue);
+            return;
+        }
+
+        let data = this._serializeForm();
+        this._templates.push({name, data});
+        this._saveTemplatesToStorage();
+        this._updateMenu();
+
+        this.new_nameTarget.value = "";
+    }
+
+    /**
+     * Export the current form content as a JSON file, which gets downloaded
+     */
+    exportTemplate()
+    {
+        let name = this.new_nameTarget.value.trim();
+        if (!name) {
+            alert(this.transNotEmptyValue);
+            return;
+        }
+
+        let data = this._serializeForm();
+        let blob = new Blob([JSON.stringify(data)], {type: 'application/json'});
+        let url = URL.createObjectURL(blob);
+
+        let a = document.createElement('a');
+        a.href = url;
+        a.download = name + ".json";
+        a.click();
     }
 
     _updateMenu()
@@ -45,19 +114,42 @@ export default class extends Controller {
 
             //Clear the menu
             menu.innerHTML = "";
-
             for (const template of this._templates) {
                 const item = document.createElement('button');
                 item.textContent = template.name;
                 item.classList.add('dropdown-item');
 
-                item.addEventListener('click', () => {
-                    this.deserializeForm(template.data);
-                });
+                //The menu selector allows to load a template, the delete menu allows to delete a template
+                if (menu === this.menuTarget) {
+                    item.addEventListener('click', () => {
+                        this.deserializeForm(template.data);
+                    });
+                } else if (menu === this.delete_menuTarget) {
+                    item.addEventListener('click', () => {
+                        this._removeTemplate(template);
+                    });
+                }
 
                 menu.appendChild(item);
             }
         });
+    }
+
+    _removeTemplate(indexOrData) {
+        if (!confirm(this.transDeleteConfirmValue)) {
+            return;
+        }
+
+        let index = indexOrData;
+        if (typeof indexOrData === 'object') {
+            index = this._templates.indexOf(indexOrData);
+        }
+
+        if (index !== -1) {
+            this._templates.splice(index, 1);
+            this._saveTemplatesToStorage();
+            this._updateMenu();
+        }
     }
 
     importJSON() {
@@ -77,6 +169,5 @@ export default class extends Controller {
         });
 
         input.click();
-
     }
 }
