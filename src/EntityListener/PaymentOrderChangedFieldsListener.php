10b95dcf0d80b426c35590f8824e4aa53fbcb112
Jan Böhmer <mail@jan-boehmer.de>
2024-10-27 15:15:01 +0100

    Added all relevant fields which should be tracked in PaymentOrderChangedFieldsListener

--- src/EntityListener/PaymentOrderChangedFieldsListener.php
+++ src/EntityListener/PaymentOrderChangedFieldsListener.php
@@ -18,9 +18,27 @@ class PaymentOrderChangedFieldsListener
     private const FIELDS_WHITELIST = [
         'submitter_name',
         'submitter_email',
+        'department',
+        'funding_id',
+        'resolution_date',
         'amount',
+        'supporting_funding_id',
+        'supporting_funding_date',
         'project_name',
-        //TODO
+        'invoice_number',
+        'customer_number',
+        'comment',
+        'fsr_kom_resolution',
+
+        //Bank infos
+        'bank_info.account_owner',
+        'bank_info.street',
+        'bank_info.zip_code',
+        'bank_info.city',
+        'bank_info.iban',
+        'bank_info.bic',
+        'bank_info.bank_name',
+        'bank_info.reference',
     ];
 
     public function preUpdate(PaymentOrder $paymentOrder, PreUpdateEventArgs $eventArgs): void
